74f5eb449432552b2453bcd92d340b35706e2b95
Abhipatro07 <abhishekpatro2002@gmail.com>
2025-11-27 07:56:31 +0530

    Handle Users

--- client/src/Pages/ManageUser/ManageUser.jsx
+++ client/src/Pages/ManageUser/ManageUser.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
 import USerForm from "../../Component/UserForm/UserForm";
 import UserList from "../../Component/UserList/UserList";
 import './ManageUser.css'
+import toast from "react-hot-toast";
+import { fetchUsers } from "../../Service/UserService";
+
 const ManageUser = () => {
+    const [users , setUsers] = useState([])
+    const [loading , setLoading] = useState(false)
+
+    useEffect(() => {
+        async function loadUsers() {
+            try{
+                setLoading(true)
+                const response = await fetchUsers()
+                setUsers(response.data)
+            }
+            catch(error){
+                console.error(error);
+                toast.error("Unable to fetch users")                
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        loadUsers()
+    } , [])
     return(
         <div className="users-container text-light">
             <div className="left-column">
-                <USerForm/>
+                <USerForm setUsers={setUsers}/>
             </div>
             <div className="right-column">
-                <UserList/>
+                <UserList users={users} setUsers={setUsers}/>
             </div>
         </div>
     )
